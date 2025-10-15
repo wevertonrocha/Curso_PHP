e6a300b7ff5de0a0ea26ab357ebf3fc74d06f953
Weverton Rocha <wevertonrocha@yahoo.com>
2025-10-15 20:04:47 -0300

    Trabalhando com atualizações com bancos de dados com PDO

---
+++ pdo/delete.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$pdo = require "connect.php";
+$sql = "DELETE FROM produtos WHERE id = ?";
+
+$prepare = $pdo->prepare($sql);
+$prepare->bindParam(1, $_GET['id']);
+$prepare->execute();
+
+echo $prepare->rowCount();
